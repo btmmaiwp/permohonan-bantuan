c65fedcc1cdda86510bb426b171aa16154cf7424
Amin Adha <aminadha@gmail.com>
2024-03-05 12:34:15 +0800

    feat: add users controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
     public function index()
     {
-        return 'Index Page';
+        $users = User::select(['name', 'email'])
+            ->where('email', 'like', '%example.com')
+            ->whereNotNull('name')
+            ->whereBetween('created_at', ['2024-01-01', '2024-12-31'])
+            ->whereIn('name', ['Pemohon', 'Staff'])
+            ->get();
+
+        return response()->json($users);
     }
 }
