e24fb84716eb3944b7cae61741e2c620cdf99bc6
Amin Adha <aminadha@gmail.com>
2024-03-06 12:36:06 +0800

    eloquent pagination function

--- app/Http/Controllers/ListActiveUsersController.php
+++ app/Http/Controllers/ListActiveUsersController.php
@@ -13,22 +13,7 @@ class ListActiveUsersController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $users = User::query()
-            ->whereHas('applications', function ($squery) {
-                $squery->where(DB::raw('YEAR(created_at)'), now()->year)
-                    ->whereHas('scheme', fn ($squery) => $squery->where('status', 'active'));
-            })
-            ->get();
-
-        $users->load('applications.scheme');
-
-        // // return DB::select('select name, email from users where id = 1');
-
-        // return User::query()
-        //     ->select('users.name', 'users.email', 'schemes.name AS scheme_name')
-        //     ->leftJoin('applications', 'applications.user_id', '=', 'users.id')
-        //     ->leftJoin('schemes', 'schemes.id', '=', 'applications.scheme_id')
-        //     ->get();
+        $users = User::paginate(2);
 
         return $users;
     }
